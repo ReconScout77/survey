f21f6e59b6acc0faf59c5beb4ce27f136f429021
Kaili Nishihira and Robert Murray <kaili@>
2017-07-06 09:25:51 -0700

    Add radio buttons and confirmation for input there.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
+    var mealInput = $("input:radio[name=meal]:checked").val();
 
     $(".name").append(nameInput);
+    $(".meal").append(mealInput);
 
     $("#confirmation").show();
     event.preventDefault();
