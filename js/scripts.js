5f31b80ba1da676aa5d199bd2229cfcbcd447ad1
Kaili Nishihira and Robert Murray <kaili@>
2017-07-06 10:08:19 -0700

    Add a color input and text no longer appends.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ $(document).ready(function() {
     var nameInput = $("input#name").val();
     var mealInput = $("input:radio[name=meal]:checked").val();
     var bornInput = $("#born").val();
+    var colorInput = $("#color").val();
 
-    $(".name").append(nameInput);
-    $(".meal").append(mealInput);
-    $(".born").append(bornInput);
+    $(".name").text(nameInput);
+    $(".meal").text(mealInput);
+    $(".born").text(bornInput);
+    $(".color").text(colorInput);
 
     $("#confirmation").show();
     event.preventDefault();
