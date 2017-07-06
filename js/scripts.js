4b6483838b8f3022b81fb79ff4cbd7b349ab8e46
Kaili Nishihira and Robert Murray <kaili@>
2017-07-06 09:39:44 -0700

    Add calendar form and confirmation for input.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
     var mealInput = $("input:radio[name=meal]:checked").val();
+    var bornInput = $("#born").val();
 
     $(".name").append(nameInput);
     $(".meal").append(mealInput);
+    $(".born").append(bornInput);
 
     $("#confirmation").show();
     event.preventDefault();
